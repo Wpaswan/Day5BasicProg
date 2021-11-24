ca5027f3de9f84054258d2e5c910d5aa9870bdc4
Wpaswan <dablumcapaswan@gmail.com>
2021-11-24 03:15:22 -0800

    Leap year

---
+++ BasicProgram/LeapYear.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    public class LeapYear
+    {
+        public void Year()
+        {
+            int year;
+            Console.WriteLine("Enter a year to check wheater Leap Or Not");
+            year = Convert.ToInt32(Console.ReadLine());
+            if ((year%4)==0)
+                Console.WriteLine("{0} is a leap year");
+            else if ((year%100)==0)
+                Console.WriteLine("{0} is not a leap year");
+            else
+                Console.WriteLine("{0} is not a leap year");
+        }
+    }
+}
---
+++ BasicProgram/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BasicProgram;
+
+Console.WriteLine("Hello, World!");
+LeapYear leapYear = new LeapYear();
+leapYear.Year();
