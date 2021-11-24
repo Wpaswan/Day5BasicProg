ac7f586297032f143caba34583a0499ee9b57634
Wpaswan <dablumcapaswan@gmail.com>
2021-11-24 08:34:03 -0800

    Uc4 EvenOdd added

---
+++ BasicProgram/EvenOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    internal class EvenOdd
+    {
+        public void EvenOrOdd()
+        {
+            Console.WriteLine("Enter any number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number%2 == 0)
+            {
+                System.Console.WriteLine(number+" is even!!");
+            }
+            else
+            {
+                System.Console.WriteLine(number+" is odd!!");
+            }
+
+        }
+    }
+}
